ef347b53ee3d6ff690e5b91b7877dc1c855587c8
marek <marek.bauer123@gmail.com>
2019-11-20 10:06:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jumpking/Game.java
@@ -0,0 +1,18 @@
+package com.example.jumpking;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.View;
+
+public class Game extends View {
+
+    public Game(Context context) {
+        super(context);
+        init(context);
+    }
+
+    void init(Context context) {
+
+    }
+}
